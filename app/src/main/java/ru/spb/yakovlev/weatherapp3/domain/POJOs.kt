b1d2b4f48ba37abe2aec351ee666300ea1428f21
Aleksey Yakovlev <aleksey@yakovlev.spb.ru>
2019-03-25 16:42:35 +0300

    POJO classes added

--- app/src/main/java/ru/spb/yakovlev/weatherapp3/domain/POJOs.kt
+++ app/src/main/java/ru/spb/yakovlev/weatherapp3/domain/POJOs.kt
@@ -1,2 +1,9 @@
 package ru.spb.yakovlev.weatherapp3.domain
 
+
+data class City (val name: String, val country: String = "RU")
+
+data class CityWeather (val name: String)
+
+data class Forecast(val cityWeather: CityWeather)
+
